8da8d182758e8c7631c7d20523476b7de9085036
Marcocholla01 <marcochollapaul01@gmail.com>
2023-09-15 01:21:44 +0300

    Added create token function and saving that in cookies

---
+++ server/utils/jwtToken.js
@@ -0,0 +1,18 @@
+// create token and saving that in cookies
+const sendToken = (user, statusCode, res) => {
+  const token = user.getJwtToken();
+
+  // Options for cookies
+  const options = {
+    expires: newDate(Date.now() + 90 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+  };
+
+  res.status(statusCode).cookie(`token`, token, options).json({
+    success: true,
+    user,
+    token,
+  });
+};
+
+module.exports = sendToken;
