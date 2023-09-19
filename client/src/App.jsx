6999fd7db2266ff373c8bcaf4ca5cdad8194ef38
Marcocholla01 <marcochollapaul01@gmail.com>
2023-09-19 11:47:00 +0300

    Added Toastify

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import {LoginPage,SignupPage, HomePage, ActivationPage} from './Routes/routes'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { LoginPage, SignupPage, HomePage, ActivationPage, NotFoundPage } from './Routes/routes'
 
 function App() {
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/'element={<HomePage/>}/>
+        <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signup' element={<SignupPage />} />
         <Route path='/activation/:activation_token' element={<ActivationPage />} />
-    </Routes>
+
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
     </BrowserRouter>
   )
 }
