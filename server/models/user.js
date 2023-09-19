ccc38d8b7222334a9d924c619169b286c7df0ce8
Marcocholla01 <marcochollapaul01@gmail.com>
2023-09-19 15:41:33 +0300

    Fixed typo

--- server/models/user.js
+++ server/models/user.js
@@ -74,7 +74,7 @@ userSchema.pre("save", async function (next){
 // jwt token
 userSchema.methods.getJwtToken = function () {
   return jwt.sign({ id: this._id}, process.env.JWT_SECRET_KEY,{
-    expiresIn: process.env.JWT_EXPIRES,
+    expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
 
