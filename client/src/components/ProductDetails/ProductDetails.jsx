b605f3b32a6e6c6d582486e1b9f5b4c9bfec2867
Marcocholla01 <marcochollapaul01@gmail.com>
2023-12-12 18:25:43 +0300

    ProductDetailsInfo Ui completed

--- client/src/components/ProductDetails/ProductDetails.jsx
+++ client/src/components/ProductDetails/ProductDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "../../styles/style";
 import {
   AiFillHeart,
@@ -148,6 +148,154 @@ const ProductDetails = ({ data }) => {
                   </div>
                 </div>
               </div>
+            </div>
+          </div>
+          <ProductDetailsInfo data={data} />
+          <br />
+          <br />
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+const ProductDetailsInfo = ({ data }) => {
+  const [active, setActive] = useState(1);
+  return (
+    <div className=" bg-[#f5f6fb] px-3 800px:px-10 py-2 rounded">
+      <div className="w-full flex justify-between border-b pt-10 pb-2">
+        <div className="relative">
+          <h5
+            className={`cursor-pointer px-1 leading-5 text-[#000] text-[18px] font-[600] 800px:text-[20px]`}
+            onClick={() => setActive(1)}
+          >
+            Product Details
+          </h5>
+          {active === 1 ? (
+            <div className={`${styles.active_indicator}`}></div>
+          ) : null}
+        </div>
+        <div className="relative">
+          <h5
+            className={`cursor-pointer px-1 leading-5 text-[#000] text-[18px] font-[600] 800px:text-[20px]`}
+            onClick={() => setActive(2)}
+          >
+            Product Reviews
+          </h5>
+          {active === 2 ? (
+            <div className={`${styles.active_indicator}`}></div>
+          ) : null}
+        </div>
+        <div className="relative">
+          <h5
+            className={`cursor-pointer px-1 leading-5 text-[#000] text-[18px] font-[600] 800px:text-[20px]`}
+            onClick={() => setActive(3)}
+          >
+            Seller Information
+          </h5>
+          {active === 3 ? (
+            <div className={`${styles.active_indicator}`}></div>
+          ) : null}
+        </div>
+      </div>
+      {active === 1 ? (
+        <>
+          {" "}
+          <p className={`py-2 text-[18px] leading-8 pb-10 whitespace-pre-line`}>
+            Product details are a crucial part of any eCommerce website or
+            online marketplace. These details help the potential customers to
+            make an informed decision about the product they are interested in
+            buying. A well-written product description can also be a powerful
+            marketing tool that can help to increase sales.Product details
+            typically include information about the product's features,
+            specifications, dimensions, weight, materials, and other relevant
+            information that can help customers to understand the product
+            better. The product details section should also include high-quality
+            images and videos of the product, as well as customer reviews and
+            ratings.
+          </p>
+          <p className={`py-2 text-[18px] leading-8 pb-10 whitespace-pre-line`}>
+            Product details are a crucial part of any eCommerce website or
+            online marketplace. These details help the potential customers to
+            make an informed decision about the product they are interested in
+            buying. A well-written product description can also be a powerful
+            marketing tool that can help to increase sales.Product details
+            typically include information about the product's features,
+            specifications, dimensions, weight, materials, and other relevant
+            information that can help customers to understand the product
+            better. The product details section should also include high-quality
+            images and videos of the product, as well as customer reviews and
+            ratings.
+          </p>
+          <p className={`py-2 text-[18px] leading-8 pb-10 whitespace-pre-line`}>
+            Product details are a crucial part of any eCommerce website or
+            online marketplace. These details help the potential customers to
+            make an informed decision about the product they are interested in
+            buying. A well-written product description can also be a powerful
+            marketing tool that can help to increase sales.Product details
+            typically include information about the product's features,
+            specifications, dimensions, weight, materials, and other relevant
+            information that can help customers to understand the product
+            better. The product details section should also include high-quality
+            images and videos of the product, as well as customer reviews and
+            ratings.
+          </p>
+        </>
+      ) : null}
+
+      {active === 2 ? (
+        <p className="w-full justify-center flex items-center min-h-[40vh]">
+          {" "}
+          No Reviews Yet!
+        </p>
+      ) : null}
+      {active === 3 ? (
+        <div className="w-full block sm:flex p-5">
+          <div className="w-full sm:w-[50%]">
+            <div className="flex items-center">
+              <img
+                src={data.shop.shop_avatar.url}
+                alt=""
+                className="w-[50px] h-[50px] rounded-full"
+              />
+              <div className="pl-2">
+                <h3 className={`${styles.shop_name}`}>{data.shop.name}</h3>
+                <h5 className="pb-3 text-[15px]">
+                  ({data.shop.ratings}) Ratings
+                </h5>
+              </div>
+            </div>
+            <p className="pt-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum porta mauris id ante laoreet porttitor. Praesent varius
+              convallis dui. Sed lacus ligula, efficitur in neque quis, tempus
+              sodales urna. Vestibulum dictum, odio id elementum ornare, orci
+              leo porta ipsum, rutrum iaculis lacus massa nec ipsum. Lorem ipsum
+              dolor sit amet, consectetur adipiscing elit. Vestibulum porta
+              mauris id ante laoreet porttitor. Praesent varius convallis dui.
+              Sed lacus ligula, efficitur in neque quis, tempus sodales urna.
+              Vestibulum dictum, odio id elementum ornare, orci leo porta ipsum,
+              rutrum iaculis lacus massa nec ipsum
+            </p>
+          </div>
+          <div className="w-full sm:w-[50%] mt-5 sm:nt-0 sm:flex flex-col items-end">
+            <div className="text-left">
+              <h5 className="font-[600]">
+                Joined on: <span className="font-[500]">14 May 2023</span>
+              </h5>
+              <h5 className="font-[600] pt-3">
+                Total Products: <span className="font-[500]">1,334</span>
+              </h5>
+              <h5 className="font-[600] pt-3">
+                Total Reviews: <span className="font-[500]">131</span>
+              </h5>
+              <Link to="/products" className="inline-block">
+                <div
+                  className={`${styles.button} mt-3 !rounded-[4px] !h-[39.5px]`}
+                >
+                  <span className="text-[#fff]">Visit Shop</span>
+                </div>
+              </Link>
             </div>
           </div>
         </div>
