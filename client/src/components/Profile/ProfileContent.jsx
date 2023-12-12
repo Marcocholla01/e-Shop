f88613440a7e3184a292d36d0545334bc65fa8e2
Marcocholla01 <marcochollapaul01@gmail.com>
2023-12-13 02:52:23 +0300

    refund section componet ui completed

--- client/src/components/Profile/ProfileContent.jsx
+++ client/src/components/Profile/ProfileContent.jsx
@@ -147,8 +147,12 @@ const ProfileContent = ({ active }) => {
           <AllOrders />
         </div>
       )}
-      {/* Order  */}
-      {active === 3 && <div>Refands</div>}
+      {/* Refund Order  */}
+      {active === 3 && (
+        <div>
+          <AllRefundOrders />
+        </div>
+      )}
       {/* Order  */}
       {active === 4 && <div>{/* <AllOrders /> */}</div>}
       {/* Order  */}
@@ -170,6 +174,119 @@ const ProfileContent = ({ active }) => {
 };
 
 const AllOrders = () => {
+  const orders = [
+    {
+      _id: "745gcgdje7464535",
+      orderItems: [
+        {
+          name: "Iphone 14 pro max",
+        },
+      ],
+      totalPrice: 120,
+      orderStatus: "Processing",
+    },
+    {
+      _id: "57575784584535",
+      orderItems: [
+        {
+          name: "Iphone 14 pro max",
+        },
+      ],
+      totalPrice: 120,
+      orderStatus: "Cancelled",
+    },
+    {
+      _id: "745gcgdje749964535",
+      orderItems: [
+        {
+          name: "Iphone 14 pro max",
+        },
+        {
+          name: "mac book air",
+        },
+      ],
+      totalPrice: 420,
+      orderStatus: "Delivered",
+    },
+  ];
+
+  const columns = [
+    { field: "id", headerName: "Order ID", minWidth: 150, flex: 0.7 },
+
+    {
+      field: "status",
+      headerName: "Status",
+      minWidth: 130,
+      flex: 0.7,
+      cellClassName: (params) => {
+        return params.getValue(params.id, "status") === "Delivered"
+          ? "greenColor"
+          : "redColor";
+      },
+    },
+    {
+      field: "itemsQty",
+      headerName: "Items Qty",
+      type: "number",
+      minWidth: 130,
+      flex: 0.7,
+    },
+
+    {
+      field: "total",
+      headerName: "Total",
+      type: "number",
+      minWidth: 130,
+      flex: 0.8,
+    },
+
+    {
+      field: " ",
+      flex: 1,
+      minWidth: 150,
+      headerName: "",
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={`/user/order/${params.id}`}>
+              <Button>
+                <AiOutlineArrowRight size={20} />
+              </Button>
+            </Link>
+          </>
+        );
+      },
+    },
+  ];
+
+  const row = [];
+
+  orders &&
+    orders.forEach((item) => {
+      row.push({
+        id: item._id,
+        itemsQty: item.orderItems.length,
+        total: "US$ " + item.totalPrice,
+        status: item.orderStatus,
+      });
+    });
+
+  return (
+    <div className="pl-8 pt-1">
+      <DataGrid
+        rows={row}
+        columns={columns}
+        pageSize={10}
+        disableSelectionOnClick
+        autoHeight
+      />
+    </div>
+  );
+};
+
+const AllRefundOrders = () => {
   const orders = [
     {
       _id: "745gcgdje7464535",
