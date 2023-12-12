0d3f511f9110612d7869304799e63aae9a221dde
Marcocholla01 <marcochollapaul01@gmail.com>
2023-12-12 18:53:20 +0300

    Similar product or suggested product ui completed

---
+++ client/src/components/Product/SuggestedProducts.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import { productData } from "../../static/data";
+import styles from "../../styles/style";
+import ProductCard from "../ProductCard/ProductCard";
+
+const SuggestedProducts = ({ data }) => {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    const d =
+      productData && productData.filter((i) => i.category === data.category);
+    setProducts(d);
+  }, []);
+
+  return (
+    <div>
+      {data ? (
+        <div className={`p-4 ${styles.section}`}>
+          <h2
+            className={`${styles.heading} text-[25px] font-[500] border-b mb-5`}
+          >
+            Related Products
+          </h2>
+          <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
+            {" "}
+            {products &&
+              products.map((i, index) => <ProductCard data={i} key={index} />)}
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default SuggestedProducts;
