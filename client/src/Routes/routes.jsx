a1856ae72c058db682a6ce0336d29b8d8b43f7e0
Marcocholla01 <marcochollapaul01@gmail.com>
2023-12-12 14:34:01 +0300

    Checkout page checkout component check out steps ui

--- client/src/Routes/routes.jsx
+++ client/src/Routes/routes.jsx
@@ -7,6 +7,11 @@ import ProductPage from "../pages/ProductPage";
 import BestSellingPage from "../pages/BestSellingPage";
 import EventsPage from "../pages/EventsPage";
 import FAQPage from "../pages/FAQPage";
+import AboutUs from "../pages/AboutUs";
+import ProductDetailsPage from "../pages/ProductDetailsPage";
+import CheckoutPage from "../pages/CheckoutPage";
+import PaymentPage from "../pages/PaymentPage";
+import OrderSuccessPage from "../pages/orderSuccessPage";
 
 export {
   LoginPage,
@@ -18,4 +23,9 @@ export {
   BestSellingPage,
   EventsPage,
   FAQPage,
+  AboutUs,
+  ProductDetailsPage,
+  CheckoutPage,
+  PaymentPage,
+  OrderSuccessPage,
 };
---
+++ client/src/pages/CheckoutPage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "../components/Layout/Header";
+import Footer from "../components/Layout/Footer";
+import CheckoutSteps from "../components/Checkout/CheckoutSteps";
+import Checkout from "../components/Checkout/Checkout";
+
+const CheckoutPage = () => {
+  return (
+    <div>
+      <Header />
+      <br />
+      <br />
+      <CheckoutSteps active={1} />
+      <Checkout />
+      <br />
+      <br />
+      <Footer />
+    </div>
+  );
+};
+
+export default CheckoutPage;
