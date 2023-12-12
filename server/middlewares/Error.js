f27607ba9a904099b7be79a514674c0b213524a6
Marcocholla01 <marcochollapaul01@gmail.com>
2023-12-13 02:02:23 +0300

    Jwt error handling... Prevent from logging in the server terminal

--- server/middlewares/Error.js
+++ server/middlewares/Error.js
@@ -1,34 +1,34 @@
 const ErrorHandler = require(`./ErrorHandler`);
 
-module.exports = (err, res, req, next) =>{
-    err.statusCode = err.statusCode || 500
-    err.message = err.message || `Internal Server Error`
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || `Internal Server Error`;
 
-    // wrong MongoDb
-    if (err.name === `CastError`) {
-        const message = `Resources not Found with this id.. Invalid ${err.path}`;
-        err = new ErrorHandler(message, 400)
-    }
-    // Duplicate key error
-    if (err.code === 11000) {
-        const message = `Duplicate key ${Object.keys(err.keyValue)} Entered`;
-        err = new ErrorHandler(message, 400);
-    }
+  // wrong MongoDb
+  if (err.name === `CastError`) {
+    const message = `Resource not found with this id. Invalid ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+  // Duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate key ${Object.keys(err.keyValue)} entered`;
+    err = new ErrorHandler(message, 400);
+  }
 
-    // wrong jwt error
-    if (err.name === `JsonWebTokenError`) {
-        const message = `Your URL is invalid try again latter`;
-        err = new ErrorHandler(message, 400);
-    }
+  // wrong jwt error
+  if (err.name === `JsonWebTokenError`) {
+    const message = `Your URL is invalid. Try again later`;
+    err = new ErrorHandler(message, 400);
+  }
 
-    //if jwt expired
-    if (err.name === `TokenExpiredError`) {
-        const message = `Your URL is expired please try again later`;
-        err = new ErrorHandler(message, 400);
-    }
-    res.status(err.statusCode).json({
-        succes: false,
-        message: err.message,
-    })
+  // if jwt expired
+  if (err.name === `TokenExpiredError`) {
+    const message = `Your URL has expired. Please try again later`;
+    err = new ErrorHandler(message, 400);
+  }
 
-}
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -5,15 +5,36 @@ const User = require("../models/user");
 
 exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
-  // console.log(token);
 
   if (!token) {
-    return next(new ErrorHandler("Please login to continue", 401));
+    return res.status(401).json({ error: "Please login to continue" });
   }
 
-  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-  req.user = await User.findById(decoded.id);
+    // Check if the user exists
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
 
-  next();
+    req.user = user;
+    next();
+  } catch (error) {
+    // Handle JWT errors
+    if (error.name === "JsonWebTokenError") {
+      return res
+        .status(401)
+        .json({ error: "Invalid token, please login again" });
+    } else if (error.name === "TokenExpiredError") {
+      return res
+        .status(401)
+        .json({ error: "Token has expired, please login again" });
+    }
+
+    // Handle other errors
+    console.error(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 });
