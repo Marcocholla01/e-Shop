132f99b86476ea256502c4b0abc13688c0d5d3ba
Marcocholla01 <marcochollapaul01@gmail.com>
2023-12-12 14:32:43 +0300

    Order Success Ui

---
+++ client/src/pages/orderSuccessPage.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Footer from "../components/Layout/Footer";
+import Header from "../components/Layout/Header";
+import Lottie from "lottie-web";
+import animationData from "../Assests/animations/107043-success.json";
+
+const orderSuccessPage = () => {
+  return (
+    <div>
+      <Header />
+      {/* <Success /> */}
+
+      <h5 className="text-center mb-14 text-[25px] text-[#000000a1]">
+        Your order is successful 😍
+      </h5>
+      <Footer />
+    </div>
+  );
+};
+
+const Success = () => {
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: animationData,
+    renderSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
+    <div>
+      <Lottie options={defaultOptions} width={300} height={300} />
+      <h5 className="text-center mb-14 text-[25px] text-[#000000a1]">
+        Your order is successful 😍
+      </h5>
+      <br />
+      <br />
+    </div>
+  );
+};
+
+export default orderSuccessPage;
