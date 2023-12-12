6c060f33ea2ef6fdf4ab56f4a8d32e156c76c68b
Marcocholla01 <marcochollapaul01@gmail.com>
2023-12-12 16:57:19 +0300

    ProductDetails Component

---
+++ client/src/components/ProductDetails/ProductDetails.jsx
@@ -0,0 +1,159 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "../../styles/style";
+import {
+  AiFillHeart,
+  AiOutlineHeart,
+  AiOutlineMessage,
+  AiOutlineShoppingCart,
+} from "react-icons/ai";
+
+const ProductDetails = ({ data }) => {
+  const [count, setCount] = useState(1);
+  const [click, setClick] = useState(false);
+  const [select, setSelect] = useState(1);
+  const navigate = useNavigate();
+
+  const handleMessageSubmit = () => {
+    navigate(`/inbox?conversation=uhvshjdu3455h8`);
+  };
+
+  const decrementCount = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  };
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div className="bg-white">
+      {data ? (
+        <div className={`${styles.section} w-[90%] 800px:w-[80%]`}>
+          <div className="w-full py-5">
+            <div className="block w-full sm:flex">
+              <div className="w-full 800px:w-[50%]">
+                <img
+                  src={data.image_Url[select].url}
+                  alt=""
+                  className="w-[80%]"
+                />
+                <div className="w-full flex">
+                  <div
+                    className={`${
+                      select === 0 ? "border" : "null"
+                    } cursor-pointer`}
+                  >
+                    <img
+                      src={data?.image_Url[0].url}
+                      alt=""
+                      className="h-[200px]"
+                      onClick={() => setSelect(0)}
+                    />
+                  </div>
+                  <div
+                    className={`${
+                      select === 1 ? "border" : "null"
+                    } cursor-pointer`}
+                  >
+                    <img
+                      src={data?.image_Url[1].url}
+                      alt=""
+                      className="h-[200px]"
+                      onClick={() => setSelect(1)}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="w-full 800px:w-[50%] }">
+                <div className="w-full  800px:w-[50%] pt-5">
+                  <h1 className={`${styles.productTitle}`}>{data.name}</h1>
+                  <p>{data.description}</p>
+                  <div className="flex pt-3">
+                    <h4 className={`${styles.productDiscountPrice}`}>
+                      {data.discount_price}$
+                    </h4>
+                    <h3 className={`${styles.price}`}>
+                      {data.price ? data.price + "$" : null}
+                    </h3>
+                  </div>
+                  <div className="flex items-center mt-12 justify-between pr-3">
+                    <div>
+                      <button
+                        className="bg-gradient-to-r from-teal-400 to-teal-500 text-white font-bold rounded-l px-4 py-2 shadow-lg hover:opacity-75 transition duration-300 ease-in-out"
+                        onClick={decrementCount}
+                      >
+                        -
+                      </button>
+                      <span className="bg-gray-200 text-gray-800 font-medium px-4 py-2">
+                        {count}
+                      </span>
+                      <button
+                        className="bg-gradient-to-l from-teal-500 to-teal-400 text-white font-bold rounded-r px-4 py-2 shadow-lg hover:opacity-75 transition duration-300 ease-in-out"
+                        onClick={incrementCount}
+                      >
+                        +
+                      </button>
+                    </div>
+                    <div>
+                      {click ? (
+                        <AiFillHeart
+                          size={22}
+                          className="cursor-pointer  right-2 top-5"
+                          onClick={() => setClick(!click)}
+                          color={click ? "red" : "#333"}
+                          title="Remove from wishlist"
+                        />
+                      ) : (
+                        <AiOutlineHeart
+                          size={22}
+                          className="cursor-pointer right-2  top-5"
+                          onClick={() => setClick(!click)}
+                          color={click ? "red" : "#333"}
+                          title="Add to wishlist"
+                        />
+                      )}
+                    </div>
+                  </div>
+                  <div
+                    className={`${styles.button} !mt-6 !rounded !h-11 flex items-center`}
+                  >
+                    <span className="text-white flex items-center">
+                      Add to cart <AiOutlineShoppingCart />
+                    </span>
+                  </div>
+                  <div className="flex items-center pt-8">
+                    <img
+                      src={data.shop.shop_avatar.url}
+                      alt=""
+                      className="w-[50px] h-[50px] rounded-full mr-2"
+                    />
+                    <div>
+                      <h3 className={`${styles.shop_name} pb-1 pt-1 mr-8`}>
+                        {data.shop.name}
+                      </h3>
+                      <h5 className="pb-3 text-[15px]">
+                        ({data.shop.ratings})Ratings
+                      </h5>
+                    </div>
+                    <div
+                      className={`${styles.button} bg-[#6443d1] mt-4 !rounded !h-11`}
+                      onClick={handleMessageSubmit}
+                    >
+                      <span className="text-white flex items-center">
+                        Send Message <AiOutlineMessage className="ml-1" />
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default ProductDetails;
