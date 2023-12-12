29306bbd28ce7838f42ce7a0ac72f1a3e5f8807b
Marcocholla01 <marcochollapaul01@gmail.com>
2023-12-12 23:15:13 +0300

    Payment method Section component completed

--- client/src/components/Profile/ProfileContent.jsx
+++ client/src/components/Profile/ProfileContent.jsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
-import { AiOutlineCamera } from "react-icons/ai";
+import {
+  AiOutlineArrowRight,
+  AiOutlineCamera,
+  AiOutlineDelete,
+} from "react-icons/ai";
 import { useSelector } from "react-redux";
 import styles from "../../styles/style";
+import { Link } from "react-router-dom";
+// import { Button } from "@material-ui/core";
+// import { DataGrid } from "@material-ui/data-grid";
 
 const ProfileContent = ({ active }) => {
   const { user } = useSelector((state) => state.user);
@@ -133,8 +140,135 @@ const ProfileContent = ({ active }) => {
           </div>
         </>
       )}
+
+      {/* Order page */}
+      {active === 2 && <div>orders</div>}
+      {/* Order page */}
+      {active === 3 && <div>Refands</div>}
+      {/* Order page */}
+      {active === 4 && <div>{/* <AllOrders /> */}</div>}
+      {/* Order page */}
+      {active === 5 && <div>track order</div>}
+      {/* Payment methods */}
+      {active === 6 && (
+        <div>
+          <PaymentMethod />
+        </div>
+      )}
+      {/* Order page */}
+      {active === 7 && <div>Address</div>}
+      {/* Order page */}
+      {active === 8 && <div>Logout</div>}
     </div>
   );
 };
 
+const AllOrders = () => {
+  //   const orders = [
+  //     {
+  //       _id: "745gcgdje7464535",
+  //       orderItems: [
+  //         {
+  //           name: "Iphone 14 pro max",
+  //         },
+  //       ],
+  //       totalPrice: 120,
+  //       orderStatus: "Processing",
+  //     },
+  //   ];
+
+  //   const columns = [
+  //     { field: "id", headerName: "Order ID", minWidth: 150, flex: 0.7 },
+
+  //     {
+  //       field: "status",
+  //       headerName: "Status",
+  //       minWidth: 130,
+  //       flex: 0.7,
+  //       cellClassName: (params) => {
+  //         return params.getValue(params.id, "status") === "Delivered"
+  //           ? "greenColor"
+  //           : "redColor";
+  //       },
+  //     },
+  //     {
+  //       field: "itemsQty",
+  //       headerName: "Items Qty",
+  //       type: "number",
+  //       minWidth: 130,
+  //       flex: 0.7,
+  //     },
+
+  //     {
+  //       field: "total",
+  //       headerName: "Total",
+  //       type: "number",
+  //       minWidth: 130,
+  //       flex: 0.8,
+  //     },
+
+  //     {
+  //       field: " ",
+  //       flex: 1,
+  //       minWidth: 150,
+  //       headerName: "",
+  //       type: "number",
+  //       sortable: false,
+  //       renderCell: (params) => {
+  //         return (
+  //           <>
+  //             {/* <Link to={`/user/order/${params.id}`}>
+  //               <Button>
+  //                 <AiOutlineArrowRight size={20} />
+  //               </Button>
+  //             </Link> */}
+  //           </>
+  //         );
+  //       },
+  //     },
+  // ];
+
+  const row = [];
+
+  orders &&
+    orders.forEach((item) => {
+      row.push({
+        id: item._id,
+        itemsQty: item.cart.length,
+        total: "US$ " + item.totalPrice,
+        status: item.status,
+      });
+    });
+
+  return <div className="pl-8 pt-1"></div>;
+};
+
+const PaymentMethod = () => {
+  return (
+    <div className="w-full px-5">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-[25px] font-[600] text-[#000000ba]">
+          Payment Methods
+        </h1>
+        <div className={`${styles.button} !rounded-md`}>
+          <span className="text-[#fff]">Add New</span>
+        </div>
+      </div>
+      <br />
+      <div className="w-full bg-[#fff] h-[70px] rounded-[4px] flex items-center px-3 shadow justify-between pr-10">
+        <div className="flex items-center">
+          <img src="" alt="Visa Method" />
+          <h5 className="pl-5 font-[600]">Marcocholla Paul</h5>
+        </div>
+        <div className="pl-8 flex items-center">
+          <h6>1234 **** **** ****</h6>
+          <h5 className="pl-6">08/2023</h5>
+        </div>
+        <div className="min-w-[10%] flex justify-between pl-8">
+          <AiOutlineDelete size={25} className="cursor-pointer" />
+        </div>
+      </div>
+    </div>
+  );
+};
 export default ProfileContent;
