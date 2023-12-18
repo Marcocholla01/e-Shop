06a842c9b5f01d36bba76fb7b9b8de3d3f846e27
Marcocholla01 <marcochollapaul01@gmail.com>
2023-12-18 15:51:14 +0300

    Event model (seller)

---
+++ server/models/event.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    // required: [true, "Please enter your event product name!"],
+  },
+  description: {
+    type: String,
+    // required: [true, "Please enter your event product description!"],
+  },
+  category: {
+    type: String,
+    // required: [true, "Please choose your event product category!"],
+  },
+  start_date: {
+    type: Date,
+    required: true,
+  },
+  finish_date: {
+    type: Date,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "Running",
+  },
+  tags: {
+    type: String,
+    // required: [true, "Please enter your event product tags!"],
+  },
+  originalPrice: {
+    type: Number,
+  },
+  discountPrice: {
+    type: Number,
+    // required: [true, "Please enter your event product discount price!"],
+  },
+  stock: {
+    type: Number,
+    // required: [true, "Please enter your event product stock price!"],
+  },
+  // images: [
+  //   {
+  //     public_id: String,
+  //     url: String,
+  //   },
+  // ],
+
+  images: [
+    {
+      type: String,
+    },
+  ],
+  shopId: {
+    type: String,
+    required: true,
+  },
+  shop: {
+    type: Object,
+    required: true,
+  },
+  sold_out: {
+    type: Number,
+    default: 0,
+  },
+});
+
+eventSchema.set("timestamps", true);
+
+module.exports = mongoose.model("Event", eventSchema);
