be8bca7a0a1170039b01b0c13595d4ac6b6287de
Marcocholla01 <marcochollapaul01@gmail.com>
2024-04-01 03:28:07 +0300

    adding backend url

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,11 @@
+// export const BASE_URL =
+//   "http://127.0.0.1:1000/api/v2" || "http://localhost:1000/api/v2";
 export const BASE_URL =
-  "http://127.0.0.1:1000/api/v2" || "http://localhost:1000/api/v2";
+  "https://api-shop0-backend.vercel.app/api/v2" ||
+  "https://api-shop0-backend.vercel.app/api/v2";
 
-export const backend_url = "http://127.0.0.1:1000";
+// export const backend_url = "http://127.0.0.1:1000";
+
+export const backend_url = "https://api-shop0-backend.vercel.app/";
 
 export const SOCKETSERVER = "http://127.0.0.1:1002";
