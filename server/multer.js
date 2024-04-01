d85a7869de397ad257cb9c014b5b452b09b6f0a4
Marcocholla01 <marcochollapaul01@gmail.com>
2024-04-01 03:49:19 +0300

    muter trying

--- server/multer.js
+++ server/multer.js
@@ -2,11 +2,10 @@ const multer = require(`multer`);
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, `uploads/`);
+    cb(null, `https://api-shop0-backend.vercel.app/uploads/`);
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix =
-      Date.now() + `-` + Math.round(Math.random() * 1e9);
+    const uniqueSuffix = Date.now() + `-` + Math.round(Math.random() * 1e9);
     const filename = file.originalname.split(`.`)[0];
     cb(null, filename + `-` + uniqueSuffix + `.png`);
   },
