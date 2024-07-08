1e25360a14796ac43a7b15911c2ba41ba637a2e3
Marcocholla01 <marcochollapaul01@gmail.com>
2024-07-08 05:45:00 +0300

    "Adding cofigs to .env file"

--- client/src/config.js
+++ client/src/config.js
@@ -1,15 +1,12 @@
-export const BASE_URL =
-  "http://127.0.0.1:1000/api/v2" || "http://localhost:1000/api/v2";
-// export const BASE_URL =
-//   "https://api-shop0-backend.vercel.app/api/v2" ||
-//   "https://api-shop0-backend.vercel.app/api/v2";
+// export const BASE_URL = "http://localhost:1000/api/v2";
+export const BASE_URL = import.meta.VITE_BASE_URL;
 
-export const backend_url = "http://127.0.0.1:1000";
-
-// export const backend_url = "https://api-shop0-backend.vercel.app/";
+// export const backend_url = "http://127.0.0.1:1000";
+export const backend_url = import.meta.env.VITE_BACKEND_URL;
 
 // export const SOCKETSERVER = "http://127.0.0.1:1002";
+export const SOCKETSERVER = import.meta.env.VITE_SOCKET_SERVER_URL;
 
-export const SOCKETSERVER = "https://shopo-socket-5ezh.onrender.com/";
-
-export const LIPIA_ONLINE_REQUEST_URL = `https://lipia-api.kreativelabske.com/api/request/stk`;
+// export const LIPIA_ONLINE_REQUEST_URL = `https://lipia-api.kreativelabske.com/api/request/stk`;
+export const LIPIA_ONLINE_REQUEST_URL = import.meta.env
+  .LIPIA_ONLINE_REQUEST_URL;
