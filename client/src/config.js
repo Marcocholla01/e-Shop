c26a9deede8dc098d3c416f746beb9311437564c
Marcocholla01 <marcochollapaul01@gmail.com>
2024-07-08 05:56:39 +0300

    "Fix: undefined urls"

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://localhost:1000/api/v2";
-export const BASE_URL = import.meta.VITE_BASE_URL;
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 // export const backend_url = "http://127.0.0.1:1000";
 export const backend_url = import.meta.env.VITE_BACKEND_URL;
@@ -9,4 +9,6 @@ export const SOCKETSERVER = import.meta.env.VITE_SOCKET_SERVER_URL;
 
 // export const LIPIA_ONLINE_REQUEST_URL = `https://lipia-api.kreativelabske.com/api/request/stk`;
 export const LIPIA_ONLINE_REQUEST_URL = import.meta.env
-  .LIPIA_ONLINE_REQUEST_URL;
+  .VITE_LIPIA_ONLINE_REQUEST_URL;
+
+console.log(import.meta.env);
